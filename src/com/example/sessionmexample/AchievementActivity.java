89cfe71df5555a11aa32eee5b52745f954d2aa6a
Zhenhao Lei <zhenhaolei@sessionm.com>
2014-09-18 11:07:32 -0400

    Init SessionM Android Sample App

---
+++ src/com/example/sessionmexample/AchievementActivity.java
@@ -0,0 +1,231 @@
+package com.example.sessionmexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.sessionmexample.CustomAchievement.AchievementPresentationStyle;
+import com.example.sessionmexample.CustomAchievement.CustomAchievementListener;
+import com.sessionm.api.AchievementData;
+import com.sessionm.api.SessionListener;
+import com.sessionm.api.SessionM;
+import com.sessionm.api.SessionM.ActivityType;
+import com.sessionm.api.SessionM.State;
+import com.sessionm.api.User;
+
+/**
+ * This class shows how to handle activity lifecycle manually with SessionM SDK.
+ * If you can't extend your activity from our BaseActivity, you can make lifecycle calls directly.
+ * Simply call each of onActivityStart(), onActivityStop(), onActivityResume(), onActivityPause() from your
+ * onStart(), onStop(), onResume(), onPause() methods in your activity class.
+ *
+ * This class also shows how to show achievements and implement custom achievements.
+ * Once you've added your achievement in developer portal, you call logAction() method. You should see
+ * an additional achievement appear in your app.
+ *
+ * To add additional achievements simply create more achievements in the developer portal and then add
+ * the corresponding logAction() call in your app.
+ *
+ * For more achievements placement ideas, please checkout http://www.sessionm.com/documentation/achievement-placement.php
+ */
+public class AchievementActivity extends Activity implements CustomAchievementListener, OnClickListener {
+
+    private BadgeView portalBadger;
+
+    private CustomAchievement customAchievement;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievement);
+        View rewardsButton = this.findViewById(R.id.rewards_btn);
+        rewardsButton.setOnClickListener(this);
+        findViewById(R.id.action_btn).setOnClickListener(this);
+        findViewById(R.id.custom_action_btn).setOnClickListener(this);
+        portalBadger = new BadgeView(this, rewardsButton);
+        SessionM.getInstance().setSessionListener(new MySessionListener());
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rewards_btn:
+                if (SessionM.getInstance().getSessionState() == State.STARTED_ONLINE) {
+                    SessionM.getInstance().presentActivity(
+                            ActivityType.PORTAL);
+                } else {
+                    Toast.makeText(
+                            AchievementActivity.this,
+                            "SessionM Portal is currently unavailable.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.custom_action_btn:
+                SessionM.getInstance().logAction("custom example");
+                onUnclaimedAchievement(SessionM.getInstance(), SessionM.getInstance().getUnclaimedAchievement());
+                break;
+            case R.id.action_btn:
+                /**
+                 * Call this method whenever you want the achievement to appear.
+                 */
+                SessionM.getInstance().logAction("example");
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Handles activity lifecycle.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SessionM.getInstance().onActivityStart(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SessionM.getInstance().onActivityResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SessionM.getInstance().onActivityPause(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SessionM.getInstance().onActivityStop(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onPause();
+        if (customAchievement != null) {
+            customAchievement.dismiss(true);
+        }
+        customAchievement = null;
+    }
+
+    /**
+     * Method to present custom achievements
+     */
+    public void onUnclaimedAchievement(SessionM session, AchievementData achievement) {
+        if (achievement != null && achievement.isCustom()) {
+            ViewGroup mainLayout = (ViewGroup) AchievementActivity.this.findViewById(R.id.parent_layout);
+            if (mainLayout != null) {
+                customAchievement = (CustomAchievement) getLayoutInflater()
+                        .inflate(R.layout.custom_achievement, null);
+                customAchievement.setAchievementData(achievement);
+                customAchievement.setAchievementListener(AchievementActivity.this);
+                customAchievement.setAchievementViewGroup(mainLayout);
+                customAchievement.setAchievementPresentationStyle(AchievementPresentationStyle.SLIDE_IN_SLIDE_OUT);
+                if (session.isAutopresentMode()) {
+                    customAchievement.present();
+                }
+            }
+        } else {
+            customAchievement = null;
+        }
+    }
+
+    /**
+     * Implements SessionListener to listen on SessionM session states
+     */
+    private class MySessionListener implements SessionListener {
+        @Override
+        public void onSessionFailed(SessionM session, int error) {
+            Toast.makeText(AchievementActivity.this,
+                    "Session failed with error code: " + error,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onUserUpdated(SessionM session, final User user) {
+            /*
+             * update the button with the new achievement count
+             */
+            int unclaimedCount = user.getUnclaimedAchievementCount();
+            if (unclaimedCount <= 0) {
+                portalBadger.hide();
+            } else {
+                portalBadger.setText("" + unclaimedCount);
+                portalBadger.show();
+            }
+            if (user.isOptedOut()) {
+                Toast.makeText(AchievementActivity.this,
+                        "User is opted out",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onSessionStateChanged(SessionM session, State state) {
+            if (state == SessionM.State.STARTED_ONLINE) {
+                // When in started online you should enable your button
+                ImageButton rewardsButton = (ImageButton) ((Activity) AchievementActivity.this)
+                        .findViewById(R.id.rewards_btn);
+                rewardsButton.setImageResource(R.drawable.icn_mportal_on);
+                rewardsButton.setEnabled(true);
+                rewardsButton.setVisibility(View.VISIBLE);
+                Toast.makeText(AchievementActivity.this, "Session started",
+                        Toast.LENGTH_SHORT).show();
+
+            } else {
+                if (state == SessionM.State.STOPPED) {
+                    Toast.makeText(AchievementActivity.this, "Session stopped",
+                            Toast.LENGTH_SHORT).show();
+
+                } else if (state == SessionM.State.STARTING) {
+                    Toast.makeText(AchievementActivity.this, "Session starting",
+                            Toast.LENGTH_SHORT).show();
+                }
+                // When the session is stopped you can enable the button and
+                // change the background image.
+                ImageButton rewardsButton = (ImageButton) ((Activity) AchievementActivity.this)
+                        .findViewById(R.id.rewards_btn);
+                rewardsButton.setImageResource(R.drawable.icn_mportal_on);
+                rewardsButton.setEnabled(true);
+                rewardsButton.setVisibility(View.GONE);
+            }
+
+        }
+    }
+
+    @Override
+    public void onDismiss(CustomAchievement customAchievement) {
+        if (this.customAchievement != null) {
+            this.customAchievement.setAchievementListener(null);
+            this.customAchievement = null;
+        }
+    }
+
+    @Override
+    public void onPresent(CustomAchievement customAchievement) {
+        // called when the achievement is presented.
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ src/com/example/sessionmexample/MainActivity.java
@@ -0,0 +1,295 @@
+package com.example.sessionmexample;
+
+/**
+ * This class extends SessionM BaseActivity to shows how to integrate SessionM SDK.
+ *
+ * SessionM provides a rewards portal inside your app where users can go to learn about the achievements
+ * you have setup and redeem their mPOINTS for rewards. This portal is opened by the mPOINTS rewards button,
+ * which displays a local notification with the user's unclaimed achievement count.
+ *
+ * There are several ways you can integrate the mPOINTS rewards button, which you will see in this sample class:
+ *  - In The Navigation Drawer (Recommended)
+ *  - On The Home Screen
+ *  - With A Welcome Message
+ *
+ * Your app comes preconfigured with visitation achievements. If everything is working, you should see the "Daily Tap In"
+ * achievement the first time you start your app. These are awarded automatically when the user opens your app.
+ *
+ * You can see more achievements implementation ideas in AchievementActivity class.
+ */
+
+import java.util.HashMap;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.sessionm.api.BaseActivity;
+import com.sessionm.api.PortalButton;
+import com.sessionm.api.SessionM;
+import com.sessionm.api.SessionM.ActivityType;
+import com.sessionm.api.User;
+
+/**
+ * This class extends from SessionM BaseActivity.
+ */
+public class MainActivity extends BaseActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private MyArrayAdapter myAdapter;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mListTitles;
+    private HashMap<Integer, String> mListCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showWelcomeDialog(this);
+        /**
+         * Set up the navigation drawer
+         */
+        mTitle = mDrawerTitle = getTitle();
+        mListTitles = getResources().getStringArray(R.array.lists_array);
+        mListCount = new HashMap<Integer, String>();
+        for (int i = 0; i < mListTitles.length; i++) {
+            mListCount.put(i, "");
+        }
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        myAdapter = new MyArrayAdapter(this, R.layout.drawer_list_item, mListTitles, mListCount);
+        mDrawerList.setAdapter(myAdapter);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    /**
+     * Update navigation drawer badge.
+     */
+    protected void onResume(){
+        super.onResume();
+        updateDrawerBadge();
+    }
+    /**
+     * Override method in SessionListener which is implemented in BaseActivity to listen on user state.
+     * Each time user info is updated, update the badge value.
+     */
+    @Override
+    public void onUserUpdated(SessionM instance, User user) {
+        updateDrawerBadge();
+    }
+
+    /**
+     * Update navigation drawer badge value, which is the unclaimed achievements count.
+     */
+    private void updateDrawerBadge() {
+        int count = SessionM.getInstance().getUser().getUnclaimedAchievementCount();
+        if (count != 0) {
+            mListCount.put(1, Integer.toString(count));
+        }
+        myAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* The click listener for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = new ListFragment();
+        Bundle args = new Bundle();
+        args.putInt(ListFragment.ARG_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.main_content, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mListTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    public static class ListFragment extends Fragment {
+        public static final String ARG_NUMBER = "arg_number";
+
+        public ListFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            int i = getArguments().getInt(ARG_NUMBER);
+            View rootView = inflater.inflate(R.layout.fragment_home,
+                    container, false);
+            /**
+             * Shows how to customize portal button resource.
+             *
+             * The rewards button defaults to the mPOINTS Gift Box image, but if you wish to use another image,
+             * add the drawable resources to your project and add the following code, replacing R.drawable.sessionm
+             * with the name of your drawable.
+             */
+            PortalButton customizedPortalButton = (PortalButton) rootView.findViewById(R.id.home_logo);
+            customizedPortalButton.setResourceId(R.drawable.m_large);
+
+            switch (i) {
+                case 1:
+                    /**
+                     * Place the following code in any cell of the navigation drawer
+                     */
+                    SessionM.getInstance().presentActivity(ActivityType.PORTAL);
+                    break;
+                case 2:
+                    Intent intent = new Intent(this.getActivity(), AchievementActivity.class);
+                    startActivity(intent);
+                    break;
+                case 3:
+                    showWelcomeDialog(this.getActivity());
+                    break;
+                default:
+                    break;
+            }
+            return rootView;
+        }
+    }
+
+    /**
+     * Customized cell, add your own badge icon here
+     */
+    public class MyArrayAdapter extends ArrayAdapter<String> {
+        private final int resource;
+        private final Context context;
+        private final String[] objects;
+        private final HashMap<Integer, String> count;
+
+        public MyArrayAdapter(Context context, int resource, String[] objects, HashMap<Integer, String> count) {
+            super(context, resource, objects);
+            this.resource = resource;
+            this.context = context;
+            this.objects = objects;
+            this.count = count;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(resource, parent,
+                    false);
+            TextView textView = (TextView) rowView.findViewById(R.id.list_text);
+
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.list_icon);
+            textView.setText(objects[position]);
+            // Using TextView now, should be replaced by customized badgeView
+            TextView badgeView = (TextView) rowView.findViewById(R.id.list_count);
+            badgeView.setTextColor(Color.WHITE);
+            badgeView.setText(count.get(position));
+            String s = objects[position];
+            imageView.setImageResource(R.drawable.ic_launcher);
+            return rowView;
+        }
+    }
+
+    /**
+     * Sample code shows how to integrate the mPOINTS rewards button with the welcome dialog.
+     */
+    public static void showWelcomeDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(R.string.dialog_welcome).setTitle(R.string.dialog_title)
+                .setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        SessionM.getInstance().presentActivity(ActivityType.PORTAL);
+                    }
+                }).setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        builder.create().show();
+    }
+}
